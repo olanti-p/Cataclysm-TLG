55b8cba36a7fc3b1c1f7ec3991c2962507ad8c5a
fairyarmadillo <94415528+fairyarmadillo@users.noreply.github.com>
2024-11-07 07:36:37 -0800

    Reduce crits melee and ranged
    The check for monsters to resist crits is now the higher of their melee * 5 or their ( dodge * mods * 5 ) 1.2, with penalties on the dodge roll if they're immobilized or downed.
    
    The result is that critical hits tend to cap at around 25% of attacks against most enemies.
    
    Gun dispersion now comes with a degree of random variance, both good and bad. High per and good vision/manip scores can reduce the penalty, while the bonus variance is constant.

--- src/ranged.cpp
+++ src/ranged.cpp
@@ -122,6 +122,9 @@ static const fault_id fault_overheat_venting( "fault_overheat_venting" );
 
 static const flag_id json_flag_FILTHY( "FILTHY" );
 
+static const limb_score_id limb_score_vision( "vision" );
+static const limb_score_id limb_score_manip( "manip" );
+
 static const material_id material_budget_steel( "budget_steel" );
 static const material_id material_case_hardened_steel( "case_hardened_steel" );
 static const material_id material_glass( "glass" );
@@ -2311,6 +2314,11 @@ dispersion_sources Character::get_weapon_dispersion( const item &obj ) const
 
     dispersion.add_range( get_modifier( character_modifier_ranged_dispersion_manip_mod ) );
 
+    // Shooting is never completely deterministic.
+    // Superhuman (18 >) per can get better than +/- 200 variance.
+    double variance_min = 950.0 - ( std::min( 800.0, ( 40.0 * ( get_per() * ( ( get_limb_score( limb_score_manip ) + get_limb_score( limb_score_vision ) ) / 2.0 ) ) ) ) );
+    dispersion.add_range( rng( variance_min, -200 ) );
+    
     if( is_driving() ) {
         // get volume of gun (or for auxiliary gunmods the parent gun)
         const item *parent = has_item( obj ) ? find_parent( obj ) : nullptr;
