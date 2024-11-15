accc0fd189a8a19038276d32242afdfe4de03076
fairyarmadillo <94415528+fairyarmadillo@users.noreply.github.com>
2024-11-15 15:15:43 -0800

    Prep for release
    Linting, version def, etc.

--- src/version.cpp
+++ src/version.cpp
@@ -1,16 +1,16 @@
 #include "get_version.h" // IWYU pragma: associated
 
-#if (defined(_WIN32) || defined(MINGW)) && !defined(GIT_VERSION) && !defined(CROSS_LINUX) && !defined(_MSC_VER)
+//#if (defined(_WIN32) || defined(MINGW)) && !defined(GIT_VERSION) && !defined(CROSS_LINUX) && !defined(_MSC_VER)
 
-#ifndef VERSION
-#define VERSION "0.I"
-#endif
+//#ifndef VERSION
+#define VERSION "1.0-beta"
+//#endif
 
-#else
+//#else
 
-#include "version.h"
+//#include "version.h"
 
-#endif
+//#endif
 
 const char *getVersionString()
 {
